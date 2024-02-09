b55dcb450f2b9c03bace1481c5746f4e96d8f5ce
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-02-09 17:28:11 +0200

    feat: config eslint, stylelint

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Outstafford</title>
+      </Head>
+      <main>
+        <p>Hello</p>
+        <p></p>
+      </main>
+    </>
+  )
+}
