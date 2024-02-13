61dc9c6c52e287d608319edeeb930a87e74f5cee
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-02-13 19:52:33 +0200

    feat: add animation to call-menu-button

--- src/components/Header/ui/CallMenuButton/CallMenuButton.tsx
+++ src/components/Header/ui/CallMenuButton/CallMenuButton.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './CallMenuButton.module.scss'
+import classNames from 'classnames'
 
 const CallMenuButton = () => {
+  const [callMenu, setCallMenu] = useState(false)
+  const handleCallMenu = () => {
+    setCallMenu((prev) => !prev)
+  }
+
+  const mods = {
+    [styles.open]: callMenu,
+  }
+
   return (
-    <button className={styles['call-menu-button']}>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+    <button
+      onClick={handleCallMenu}
+      className={classNames(styles['call-menu-button'], mods)}
+    >
+      <div className={styles['call-menu-button__wrapper']}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </button>
   )
 }
--- src/components/Header/ui/Header.tsx
+++ src/components/Header/ui/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
-import styles from './Header.module.scss'
 import CallMenuButton from '@/components/Header/ui/CallMenuButton/CallMenuButton'
 import HeaderNavigation from '@/components/Header/ui/HeaderNavigation/HeaderNavigation'
 import ButtonPrimary from '@/ui/ButtonPrimary/ButtonPrimary'
+
+import styles from './Header.module.scss'
 
 const Header = () => {
   return (
