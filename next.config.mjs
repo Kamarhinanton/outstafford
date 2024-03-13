d9b35b2da5d8db8e1280addecd2bac809f9d833c
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-03-13 12:50:47 +0200

    feat: add some config modes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
+      devtool: 'source-map',
     })
 
     return config
