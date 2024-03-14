2e8392bbc42ae3aaa6861140b291fb3ba45d48ab
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-03-14 19:48:19 +0200

    feat: add benefits section

---
+++ src/modules/Careers/ui/Benefits/Benefits.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Container from '@/app/layouts/Container'
+import classNames from 'classnames'
+import { BackgroundImage } from '@/ui/BackgroundImage/BackgroundImage'
+
+import styles from './Benefits.module.scss'
+
+const benefitsData = [
+  {
+    title: 'Supportive Team',
+    description: 'Supportive Team',
+  },
+  {
+    title: 'Free Courses',
+    description: 'Supportive Team',
+    image: '/images/Careers/1.png',
+  },
+  {
+    title: 'Regular Bonuses',
+    description: 'Supportive Team',
+  },
+  {
+    title: 'MacBooks and iMacs',
+  },
+  {
+    title: 'Paid Vacations',
+    description: 'Paid Vacations',
+    image: '/images/Careers/3.png',
+  },
+  {
+    title: 'Free Health Insurance',
+    description: 'Paid Vacations',
+    image: '/images/Careers/2.png',
+  },
+]
+
+const Benefits = () => {
+  return (
+    <section className={styles['benefits']}>
+      <Container>
+        <div className={styles['benefits__content']}>
+          <h2 className={classNames('h1', styles['title'])}>
+            Our <span>benefits</span>
+          </h2>
+          <ul className={styles['benefits__content_list']}>
+            {benefitsData.map((item) => (
+              <li className={styles['item']} key={item.title}>
+                {item.image && (
+                  <BackgroundImage
+                    className={styles['item__image']}
+                    position={'cover'}
+                    src={item.image}
+                    alt={'picture'}
+                  />
+                )}
+                <h3 className={classNames('h3', styles['item__title'])}>
+                  {item.title}
+                </h3>
+                {item.description && (
+                  <p className={styles['item__description']}>
+                    {item.description}
+                  </p>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+export default Benefits
--- src/modules/Careers/ui/CareersContent.tsx
+++ src/modules/Careers/ui/CareersContent.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '@/components/Footer/Footer'
 import HeroCareers from '@/modules/Careers/ui/HeroCareers/HeroCareers'
 import Positions from '@/modules/Careers/ui/Positions/Positions'
+import Benefits from '@/modules/Careers/ui/Benefits/Benefits'
 
 const CareersContent = () => {
   return (
     <main>
       <HeroCareers />
       <Positions />
+      <Benefits />
       <Footer />
     </main>
   )
