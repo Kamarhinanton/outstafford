f887110e1e4813ccc4cd67c0e3967b0f373d42cc
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-03-28 17:28:41 +0200

    feat: add mosaic case

--- src/modules/Case/ui/CaseContent.tsx
+++ src/modules/Case/ui/CaseContent.tsx
@@ -2,12 +2,50 @@ import React from 'react'
 import HeroCaseSection from '@/modules/Case/ui/HeroCaseSection/HeroCaseSection'
 import Footer from '@/components/Footer/Footer'
 import ChallengesCase from '@/modules/Case/ui/ChallengesCase/ChallengesCase'
+import MosaicSection from '@/modules/Case/ui/MosaicSection/MosaicSection'
 
+const mosaicData1 = {
+  description: {
+    title: 'The story',
+    textTop:
+      'Space Needle visitors can now access their photos and videos within seconds, eliminating the previous 10-15 minute wait time. As a result, user satisfaction has increased, leading to a 20% boost in app usage within the first month. Space Needle visitors can now access their photos and videos within seconds, eliminating the previous 10-15 minute wait time. As a result, user satisfaction has increased, leading to a 20% boost in app usage within the first month.',
+  },
+  mosaic: {
+    topImg: '/images/Home/preview.jpg',
+    bottomImg: '/images/Home/preview.jpg',
+    doubleImg: {
+      img1: '/images/Home/preview.jpg',
+      img2: '/images/Home/preview.jpg',
+    },
+  },
+}
+
+const mosaicData2 = {
+  description: {
+    title: 'Results',
+    textTop:
+      'Space Needle visitors can now access their photos and videos within seconds, eliminating the previous 10-15 minute wait time. As a result, user satisfaction has increased, leading to a 20% boost in app usage within the first month. Space Needle visitors can now access their photos and videos within seconds, eliminating the previous 10-15 minute wait time. ',
+    quote: {
+      text: 'Space Needle visitors can now access their photos and videos within seconds, eliminating the previous 10-15 minute wait time. As a result, user satisfaction has increased, leading to a 20% boost in app usage within the first month.',
+      author: 'Artem Larin',
+      position: 'CEO of Outstafford',
+      img: '/images/Home/preview.jpg',
+    },
+    textBottom:
+      'Space Needle visitors can now access their photos and videos within seconds, eliminating the previous 10-15 minute wait time. As a result, user satisfaction has increased, leading to a 20% boost in app usage within the first month. Space Needle visitors can now access their photos and videos within seconds, eliminating the previous 10-15 minute wait time.',
+  },
+  mosaic: {
+    topImg: '/images/Home/preview.jpg',
+    bottomImg: '/images/Home/preview.jpg',
+  },
+}
 const CaseContent = () => {
   return (
     <main>
       <HeroCaseSection />
       <ChallengesCase />
+      <MosaicSection mosaicData={mosaicData1} />
+      <MosaicSection mosaicData={mosaicData2} />
       <Footer />
     </main>
   )
---
+++ src/modules/Case/ui/MosaicSection/MosaicSection.tsx
@@ -0,0 +1,126 @@
+import React, { FC } from 'react'
+import Container from '@/app/layouts/Container'
+import classNames from 'classnames'
+import { BackgroundImage } from '@/ui/BackgroundImage/BackgroundImage'
+
+import styles from './MosaicSection.module.scss'
+
+type MosaicDataType = {
+  description?: {
+    title?: string
+    textTop?: string
+    quote?: {
+      text?: string
+      author?: string
+      position?: string
+      img?: string
+    }
+    textBottom?: string
+  }
+  mosaic?: {
+    topImg?: string
+    bottomImg?: string
+    doubleImg?: {
+      img1?: string
+      img2?: string
+    }
+  }
+}
+
+type MosaicSectionType = {
+  mosaicData: MosaicDataType
+}
+
+const MosaicSection: FC<MosaicSectionType> = ({ mosaicData }) => {
+  return (
+    <section className={styles['mosaic']}>
+      <Container>
+        <div className={styles['mosaic__content']}>
+          <div className={styles['top-description']}>
+            <h2 className={classNames('h2', styles['top-description__title'])}>
+              {mosaicData.description?.title}
+            </h2>
+            <div className={styles['top-description__text']}>
+              <p>{mosaicData.description?.textTop}</p>
+              {mosaicData.description?.quote && (
+                <div className={styles['quote']}>
+                  <p className={styles['quote__top']}>
+                    {mosaicData.description.quote.text}
+                  </p>
+                  <div className={styles['quote__bottom']}>
+                    {mosaicData.description.quote.img && (
+                      <BackgroundImage
+                        src={mosaicData.description.quote.img}
+                        alt={'author'}
+                        position={'cover'}
+                        className={styles['quote__bottom_img']}
+                      />
+                    )}
+                    {mosaicData.description.quote.author && (
+                      <div className={styles['quote__bottom_naming']}>
+                        {mosaicData.description.quote.author && (
+                          <p>{mosaicData.description.quote.author}</p>
+                        )}
+                        {mosaicData.description.quote.position && (
+                          <p>
+                            <span>{mosaicData.description.quote.position}</span>
+                          </p>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
+              <p>{mosaicData.description?.textBottom}</p>
+            </div>
+          </div>
+          <div
+            className={classNames(
+              styles['bottom-description'],
+              !mosaicData.mosaic?.doubleImg ? styles['no-double'] : '',
+            )}
+          >
+            {mosaicData.mosaic?.topImg && (
+              <BackgroundImage
+                src={mosaicData.mosaic.topImg}
+                alt={'picture'}
+                className={styles['image']}
+                position={'cover'}
+              />
+            )}
+            {mosaicData.mosaic?.doubleImg && (
+              <div className={styles['double-images']}>
+                {mosaicData.mosaic?.doubleImg?.img1 && (
+                  <BackgroundImage
+                    src={'/images/Home/preview.jpg'}
+                    alt={'picture'}
+                    className={styles['image']}
+                    position={'cover'}
+                  />
+                )}
+                {mosaicData.mosaic?.doubleImg?.img2 && (
+                  <BackgroundImage
+                    src={'/images/Home/preview.jpg'}
+                    alt={'picture'}
+                    className={styles['image']}
+                    position={'cover'}
+                  />
+                )}
+              </div>
+            )}
+            {mosaicData.mosaic?.bottomImg && (
+              <BackgroundImage
+                src={'/images/Home/preview.jpg'}
+                alt={'picture'}
+                className={classNames(styles['image'], styles['mob'])}
+                position={'cover'}
+              />
+            )}
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+export default MosaicSection
