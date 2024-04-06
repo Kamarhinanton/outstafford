7015803976388950cc1bf8160a555367cf9856a6
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-04-06 11:57:57 +0300

    feat: add hero top

--- src/modules/About/ui/HeroAbout/AboutHero.tsx
+++ src/modules/About/ui/HeroAbout/AboutHero.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Container from '@/app/layouts/Container'
+import { BackgroundImage } from '@/ui/BackgroundImage/BackgroundImage'
 
 import styles from './AboutHero.module.scss'
 
 const AboutHero = () => {
   return (
-    <section>
+    <section className={styles['about']}>
       <Container>
-        <h1>hello</h1>
+        <div className={styles['about__content']}>
+          <BackgroundImage
+            className={styles['about__content_logo']}
+            src={'/images/logo.png'}
+            alt={'logo'}
+          />
+          <h1>
+            <span>Outstafford</span>
+          </h1>
+          <h2>
+            Full-stack design and development
+            <br /> team. We build complex
+            <span>mobile apps</span>
+            <br />
+            and progressive <span>web apps</span>
+            with
+            <span>AI integrations.</span>
+          </h2>
+        </div>
       </Container>
     </section>
   )
