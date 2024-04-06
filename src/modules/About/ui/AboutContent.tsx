9ebb9c6b410bcd8b6f4375eb7850dcad43cbdd89
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-04-06 16:06:57 +0300

    feat: add founded section

--- src/modules/About/ui/AboutContent.tsx
+++ src/modules/About/ui/AboutContent.tsx
@@ -3,11 +3,13 @@ import AboutHero from '@/modules/About/ui/HeroAbout/AboutHero'
 import Footer from '@/components/Footer/Footer'
 import CTA from '@/components/CTA/CTA'
 import ExploreMoreProjects from '@/modules/Case/ui/ExploreMoreProjects/ExploreMoreProjects'
+import FoundedSection from '@/modules/About/ui/FoundedSection/FoundedSection'
 
 const AboutContent = () => {
   return (
     <main>
       <AboutHero />
+      <FoundedSection />
       <ExploreMoreProjects />
       <CTA />
       <Footer />
---
+++ src/modules/About/ui/FoundedSection/FoundedSection.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Container from '@/app/layouts/Container'
+
+import styles from './FoundedSection.module.scss'
+
+const data = [
+  {
+    title: '2018',
+    description: 'year founded',
+  },
+  {
+    title: 'Charlotte, NC',
+    description: 'based',
+  },
+  {
+    title: '30',
+    description: 'members worldwide',
+  },
+]
+
+const FoundedSection = () => {
+  return (
+    <section className={styles['founded']}>
+      <Container size={'small'}>
+        <ul className={styles['founded__content']}>
+          {data.map((item) => (
+            <li key={item.title} className={styles['founded__content_item']}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </section>
+  )
+}
+
+export default FoundedSection
