cb3c6de3b5480cd00904be771a102542c55bc318
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-04-09 22:17:57 +0300

    feat: add we create desktop

--- src/modules/About/ui/LottieLine/LottieLine.tsx
+++ src/modules/About/ui/LottieLine/LottieLine.tsx
@@ -1,17 +1,25 @@
-import React, { useRef } from 'react'
+import React, { FC, useRef } from 'react'
 import { useMotionValueEvent, useScroll, useTransform } from 'framer-motion'
 import Lottie, { LottieRefCurrentProps } from 'lottie-react'
 import loaderJson from './data/line.json'
+import classNames from 'classnames'
+
 import styles from './LottieLine.module.scss'
 
-const LottieLine = () => {
+type LottieLineType = {
+  src?: unknown
+  className?: string
+  fps?: number
+}
+
+const LottieLine: FC<LottieLineType> = ({ src, className, fps = 26 }) => {
   const targetRef = useRef<HTMLDivElement | null>(null)
   const refLottie = useRef<LottieRefCurrentProps | null>(null)
   const { scrollYProgress } = useScroll({
     target: targetRef,
     offset: ['start end', 'end center'],
   })
-  const progress = useTransform(scrollYProgress, [0, 1], [0, 26])
+  const progress = useTransform(scrollYProgress, [0, 1], [0, fps])
 
   useMotionValueEvent(progress, 'change', (latest) => {
     const roundedLatest = Math.round(latest)
@@ -19,11 +27,11 @@ const LottieLine = () => {
   })
 
   return (
-    <div ref={targetRef} className={styles['line']}>
+    <div ref={targetRef} className={classNames(styles['line'], className)}>
       <Lottie
         className={styles['line__lottie']}
         autoplay={false}
-        animationData={loaderJson}
+        animationData={src ? src : loaderJson}
         lottieRef={refLottie}
       />
     </div>
