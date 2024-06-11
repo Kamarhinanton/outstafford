ae28ab920d4c665d589b7894c64c64075dad7018
Kamarhinanton <Kamarhin_anton@ukr.net>
2024-06-11 17:21:59 +0300

    fix: add styles to portfolio home[2]

--- src/modules/Home/ui/OurPortfolio/OurPortfolio.tsx
+++ src/modules/Home/ui/OurPortfolio/OurPortfolio.tsx
@@ -4,30 +4,34 @@ import Link from 'next/link'
 import classNames from 'classnames'
 import routes from '@/utils/routes'
 import { BackgroundImage } from '@/ui/BackgroundImage/BackgroundImage'
+import CardTransformPerspective from '@/ui/CardTransformPerspective/CardTransformPerspective'
 
 import styles from './OurPortfolio.module.scss'
 const OurPortfolio = () => {
   return (
     <section className={styles['our-portfolio']}>
-      <Container size={'small'}>
-        <div className={styles['our-portfolio__content']}>
-          <Link
-            scroll={false}
-            className={styles['our-portfolio__content_link']}
-            href={routes.public.projects}
-          >
-            <BackgroundImage
-              src={'/images/Projects/MyAI/m-4.jpg'}
-              alt={'picture'}
-              className={styles['image']}
-              position={'cover'}
-              quality={100}
-            />
-            <h2 className={classNames(styles['title'], 'h2')}>
-              Explore our Projects
-            </h2>
-          </Link>
-        </div>
+      <Container className={styles['wrapper']} size={'small'}>
+        <Link scroll={false} href={routes.public.projects}>
+          <div className={styles['our-portfolio__content']}>
+            <CardTransformPerspective
+              cursor={false}
+              className={styles['our-portfolio__content_wrapper']}
+              rotateRangeX={['10deg', '-10deg']}
+              rotateRangeY={['-10deg', '10deg']}
+            >
+              <BackgroundImage
+                src={'/images/Projects/MyAI/m-4.jpg'}
+                alt={'picture'}
+                className={styles['image']}
+                position={'cover'}
+                quality={100}
+              />
+              <h2 className={classNames(styles['title'], 'h2')}>
+                Explore our Projects
+              </h2>
+            </CardTransformPerspective>
+          </div>
+        </Link>
       </Container>
     </section>
   )
